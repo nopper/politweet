90189af37e18098972dfc39b37b62d294e23a1de
Francesco Piccinno <stack.box@gmail.com>
2011-11-18 00:05:37 +0100

    Finishing implementation. update tool still missing

--- persistence.py
+++ persistence.py
@@ -1,38 +1,44 @@
 import json
 import os.path
 import sqlite3
+from time import mktime, strptime
 
 tcreation = """
 CREATE TABLE tweets(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 
-    tid TEXT UNIQUE,
-    text TEXT
+    time INTEGER,
+    rtcount INTEGER,
+    uid INTEGER,
+    tid INTEGER UNIQUE,
+    text TEXT,
+    geo TEXT
 );
 CREATE TABLE retweets(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 
-    rtid TEXT UNIQUE,
+    rtime INTEGER,
+    ruid INTEGER,
+    rtid INTEGER UNIQUE,
     rtext TEXT,
     rgeo TEXT,
 
-    authortid TEXT UNIQUE,
-    tid TEXT UNIQUE,
-    text TEXT,
-    geo TEXT
+    otime INTEGER,
+    ouid INTEGER,
+    otid INTEGER,
+    otext TEXT,
+    ogeo TEXT
 );
 CREATE TABLE users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 
     uid TEXT UNIQUE,
-    screen_name TEXT UNIQUE,
-    geo TEXT
+    screen_name TEXT UNIQUE
 );
 CREATE TABLE followers(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-    src_uid TEXT UNIQUE,
-    dst_uid TEXT UNIQUE
+    src_uid TEXT,
+    dst_uid TEXT,
+    PRIMARY KEY(src_uid, dst_uid)
 );
 """
 
@@ -40,18 +46,23 @@ dcreation = """
 CREATE TABLE pages(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 
+    type INTEGER,
+    uid TEXT,
     tid TEXT UNIQUE,
     json TEXT
 );
 """
 
 class Collector(object):
-    def __init__(self):
-        missing_dumps = (os.path.exists("dumps.db") == False)
-        missing_tweets = (os.path.exists("tweets.db") == False)
+    DUMPS_DB = 'db/dumps.db'
+    TWEETS_DB = 'db/tweets.db'
 
-        self.dumps = sqlite3.connect('db/dumps.db')
-        self.tweets = sqlite3.connect('db/tweets.db')
+    def __init__(self):
+        missing_dumps = (os.path.exists(Collector.DUMPS_DB) == False)
+        missing_tweets = (os.path.exists(Collector.TWEETS_DB) == False)
+
+        self.dumps = sqlite3.connect(Collector.DUMPS_DB)
+        self.tweets = sqlite3.connect(Collector.TWEETS_DB)
 
         self.dcursor, self.tcursor = self.dumps.cursor(), self.tweets.cursor()
 
@@ -65,21 +76,136 @@ class Collector(object):
     def _execute(self, cursor, q):
         cursor.executescript(q)
 
-    def save_json_obj(self, jsonobj):
-        if not isinstance(jsonobj, (list, tuple)):
-            coll = (jsonobj, )
+    def get_user_id(self, screen_name):
+        self.tcursor.execute(
+            "SELECT uid FROM users WHERE screen_name=? LIMIT 1",
+            (screen_name, )
+        )
 
-        for obj in coll:
-            self.dcursor.execute(
-                "INSERT INTO pages(tid, json) VALUES(?, ?)",
-                (obj['id_str'], json.dumps(obj))
+        for row in self.tcursor:
+            try:
+                return int(row[0])
+            except:
+                return -1
+
+    def get_missing_uid(self):
+        cursor = self.tweets.cursor()
+        for row in cursor.execute(
+            "SELECT dst_uid FROM followers WHERE " \
+            "dst_uid NOT IN (SELECT uid FROM users) GROUP BY dst_uid"):
+            yield (row[0])
+
+
+    def get_followers_for(self, uid):
+        cursor = self.tweets.cursor()
+        for row in cursor.execute(
+            "SELECT src_uid FROM followers WHERE dst_uid=?", (str(uid), )):
+            yield (row[0])
+
+    def save_followers(self, leader, ids):
+        for id in ids:
+            self.tcursor.execute(
+                "INSERT OR IGNORE INTO followers(src_uid, dst_uid) " \
+                "VALUES (?, ?)", (id, leader)
             )
+
+    def save_user_infos(self, jsonobj):
+        self.save_json_obj(jsonobj, type=2)
+
+        for user in jsonobj:
+            self.tcursor.execute(
+                "INSERT INTO users(uid, screen_name) VALUES (?, ?)",
+                (user['id_str'], user['screen_name'])
+            )
+
+    def save_json_obj(self, jsonobj, type=0):
+        if not isinstance(jsonobj, (list, tuple)):
+            jsonobj = (jsonobj, )
+
+        for obj in jsonobj:
+            try:
+                self.dcursor.execute(
+                    "INSERT INTO pages(type, uid, tid, json) VALUES(?,?,?,?)",
+                    (type, obj['user']['id_str'],
+                     obj['id_str'], json.dumps(obj))
+                )
+            except Exception:
+                print("Skipping tid={:s}. Already present".format(
+                      obj['id_str']))
+
+    def save_tweet(self, jsonobj):
+        # NB: to convert it back datetime.datetime.fromtimestamp(time.time())
+        if not isinstance(jsonobj, (list, tuple)):
+            jsonobj = (jsonobj, )
+
+        self.save_json_obj(jsonobj, type=0)
+
+        for tweet in jsonobj:
+            rtcount = tweet['retweet_count']
+            uid     = tweet['user']['id_str']
+            tid     = tweet['id_str']
+            txt     = tweet['text'].encode('utf-8')
+            geo     = json.dumps(tweet['geo'])
+            time    = mktime(strptime(tweet['created_at'],
+                                      '%a %b %d %H:%M:%S +0000 %Y'))
+
+            try:
+                self.tcursor.execute(
+                    "INSERT INTO tweets(time, rtcount, uid, tid, text, geo) " \
+                    "VALUES (?, ?, ?, ?, ?, ?)",
+                    (time, rtcount, uid, tid, txt, time, geo)
+                )
+            except:
+                print("Skipping tweet. tid={:s} already present".format(tid))
+
+            self.tcursor.execute(
+                "INSERT OR REPLACE INTO users(uid, screen_name) " \
+                "VALUES (?, ?)", (uid, tweet['user']['screen_name']))
+
+    def save_retweet(self, jsonobj):
+        if not isinstance(jsonobj, (list, tuple)):
+            jsonobj = (jsonobj, )
+
+        self.save_json_obj(jsonobj, type=1)
+
+        for tweet in jsonobj:
+            ruid  = tweet['user']['id_str']
+            rtid  = tweet['id_str']
+            rtext = tweet['text']
+            rgeo  = json.dumps(tweet['geo'])
+            rtime = mktime(strptime(tweet['created_at'],
+                                      '%a %b %d %H:%M:%S +0000 %Y'))
+
+            ouid  = tweet['retweeted_status']['user']['id_str']
+            otid  = tweet['retweeted_status']['id_str']
+            otext = tweet['retweeted_status']['text']
+            ogeo  = json.dumps(tweet['retweeted_status']['geo'])
+            otime = mktime(strptime(tweet['retweeted_status']['created_at'],
+                                      '%a %b %d %H:%M:%S +0000 %Y'))
+
+            try:
+                self.tcursor.execute(
+                    "INSERT INTO retweets(rtime, ruid, rtid, rtext, rgeo, " \
+                                         "otime, ouid, otid, otext, ogeo) " \
+                                         "VALUES (?,?,?,?,?,?,?,?,?,?)",    \
+                    (rtime, ruid, rtid, rtext, rgeo,
+                     otime, ouid, otid, otext, ogeo)
+                )
+
+            except Exception:
+                print("Skipping tweet. rtid={:s} already present".format(rtid))
+
+            # Also update the uid <=> screen_name mappings
+
+            self.tcursor.execute(
+                "INSERT OR REPLACE INTO users(uid, screen_name) " \
+                "VALUES (?, ?)", (ruid, tweet['user']['screen_name']))
+
+            self.tcursor.execute(
+                "INSERT OR REPLACE INTO users(uid, screen_name) " \
+                "VALUES (?, ?)", (ouid,
+                tweet['retweeted_status']['user']['screen_name']))
 
     def save(self):
         self.dumps.commit()
         self.tweets.commit()
-
-if __name__ == "__main__":
-    c = Collector()
-    c.save_json_obj({'id_str': '123564524352', 'miao': 'fuffa'})
-    c.save()
--- tweetdump.py
+++ tweetdump.py
@@ -1,55 +1,194 @@
 import sys
 import json
+import time
 import socks
 import httplib2
-from time import mktime, strptime
-
-URL = "http://twitter.com/statuses/user_timeline/{:s}.json?page={:d}&count=100"
+from persistence import Collector
 
 class TweetDumper(object):
-    def __init__(self, url):
-        self.url = url
+    ARGS = ('user', 'page')
+    DESC = "Retrieve tweets of <user> starting from <page>"
+
+    METHOD = 'save_tweet'
+    URL = "http://twitter.com/statuses/user_timeline/" \
+          "{:s}.json?page={:d}&count=100"
+
+    def __init__(self):
+        self.url = self.URL
+        self.collector = Collector()
         self.invoker = httplib2.Http(
             proxy_info=httplib2.ProxyInfo(socks.PROXY_TYPE_SOCKS5,
                                           'localhost', 9050))
 
-    def dump(self, politician, page=1, fmt='{:s}-history'):
-        tweetdb = open(fmt.format(politician), 'a')
+    def dump(self, politician, page=1):
+        try:
+            while True:
+                print("Retrieving tweets at page {:d}".format(page))
+
+                response, content = self.invoker.request(
+                        self.url.format(politician, page),
+                        'GET', headers={'User-Agent:': 'Google Chrome'})
+
+                if response['status'] != '200':
+                    print self.url.format(politician, page)
+                    print("Error at page {:d}. Limit exceeded".format(page))
+                    break
+
+                collection = json.loads(content)
+
+                if len(collection) == 0:
+                    break
+
+                meth = getattr(self.collector, self.METHOD)
+                meth.__call__(collection)
+
+                page += 1
+        finally:
+            print("Committing changes to the database")
+            self.collector.save()
+
+
+class RetweetDumper(TweetDumper):
+    DESC = "Retrieve retweets of <user> starting from <page>"
+    METHOD = 'save_retweet'
+
+    URL = "http://api.twitter.com/1/statuses/retweeted_by_user.json" \
+          "?screen_name={:s}&count=100&page={:d}"
+
+class FollowerDumper(TweetDumper):
+    ARGS = ('user', 'cursor')
+    DESC = "Retrieves the followers of <user> starting from <cursor>"
+
+    METHOD = 'save_friend'
+    URL = "http://api.twitter.com/1/followers/ids.json?" \
+          "user_id={:d}&cursor={:d}&stringify_ids=true"
+
+    def dump(self, politician, cursor=-1):
+        if isinstance(politician, basestring):
+            politician = self.collector.get_user_id(politician)
+
+        print("Downloading followers of {:d}".format(politician))
 
         while True:
-            print("Retrieving tweets at page {:d}".format(page))
-
             response, content = self.invoker.request(
-                    self.url.format(politician, page),
+                    self.url.format(politician, cursor),
                     'GET', headers={'User-Agent:': 'Google Chrome'})
 
             if response['status'] != '200':
-                print self.url.format(politician, page)
-                print("Error at page {:d}. Limit exceeded".format(page))
+                print(self.url.format(politician, cursor))
+                print("Error at cursor {:d}. Limit exceeded.".format(cursor))
+                print("Sleeping 2 seconds. Please change tor identity")
+                time.sleep(2)
+                continue
+
+            body = json.loads(content)
+
+            ids = body['ids']
+
+            if len(ids) == 0:
                 break
+
+            cursor = int(body['next_cursor_str'])
+            self.store(str(politician), ids)
+
+            print("Saving {:d} connections".format(len(ids)))
+
+        print("Committing changes to the database")
+        self.collector.save()
+
+    def store(self, politician, ids):
+        self.collector.save_followers(politician, ids)
+
+class SecondLevelDumper(FollowerDumper):
+    ARGS = ('user', '2nd-useridx')
+    DESC = "Retrieves the 2nd-level followers of <user> starting " \
+           "from <2nd-useridx>"
+
+    def dump(self, politician, page=-1):
+        pid = self.collector.get_user_id(politician)
+        self.pid = pid
+
+        for idx, uid in enumerate(self.collector.get_followers_for(pid)):
+            if idx < page:
+                continue
+
+            print("Dumping user at index {:d}".format(page))
+            FollowerDumper.dump(self, int(uid), -1)
+
+    def store(self, dst_uid, ids):
+        # Here we need to save only followers which are already present
+        ids = filter(
+            lambda src_uid: self.collector.is_following(src_uid, self.pid),
+            ids
+        )
+        self.collector.save_followers(dst_uid, ids)
+
+
+class UserLookups(TweetDumper):
+    """
+    For the docs and the lulz please refer to:
+        https://dev.twitter.com/docs/api/1/get/users/lookup
+    """
+    ARGS = ()
+    DESC = "Fill missing user info information such as screen_name"
+    URL = "http://api.twitter.com/1/users/lookup.json?user_id={:s}"
+
+    def dump(self):
+        tmplist = []
+        for uid in self.collector.get_missing_uid():
+            tmplist.append(uid)
+
+            if len(tmplist) < 100:
+                continue
+
+            self.retrieve_list(tmplist)
+            tmplist = []
+
+        if tmplist:
+            self.retrieve_list(tmplist)
+
+        print("Committing changes to the database")
+        self.collector.save()
+
+    def retrieve_list(self, tmplist):
+        while True:
+            response, content = self.invoker.request(
+                    self.url.format(','.join(tmplist)),
+                    'GET', headers={'User-Agent:': 'Google Chrome'})
+
+            if response['status'] != '200':
+                #print self.url.format(','.join(tmplist))
+                print("Error at while retrieving user info. Limit exceeded.")
+                print("Sleeping 2 seconds. Please change tor identity")
+                time.sleep(2)
+                continue
 
             collection = json.loads(content)
+            self.collector.save_user_infos(collection)
+            break
 
-            if len(collection) == 0:
-                print("No more tweets at page {:d}".format(page))
-                break
-
-            for tweet in collection:
-                txt = tweet['text'].encode('utf-8')
-                tweetid = tweet['id_str']
-                timeint = mktime(strptime(tweet['created_at'],
-                                 '%a %b %d %H:%M:%S +0000 %Y'))
-
-                tweetdb.write("{:s} {:d} {:s}\n".format(
-                              tweetid, int(timeint),
-                              txt.translate(None, '\n\t'))
-                )
-
-            page += 1
+            print("Saving {:d} profiles".format(len(collection)))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage {:s} <politician> <startpage>".format(sys.argv[0]))
-        sys.exit(-1)
+    tools = {
+        'tweet': TweetDumper,
+        'retweet': RetweetDumper,
+        'follower': FollowerDumper,
+        '2follower': SecondLevelDumper,
+        'infolookup': UserLookups,
+    }
 
-    TweetDumper(URL).dump(sys.argv[1], int(sys.argv[2]))
+    try:
+        tool = tools[sys.argv[1]]
+        if len(sys.argv) - 2 != len(tool.ARGS):
+            raise Exception("love waste")
+    except:
+        for name, tool in sorted(tools.items()):
+            args = ' '.join(map(lambda x: '<{:s}>'.format(x), tool.ARGS))
+            print("[+] {:s} {:s} {:s}".format(sys.argv[0], name, args))
+            print("")
+            print("    {:s}".format(tool.DESC))
+            print("")
+        sys.exit(-1)
+    else:
+        tools[sys.argv[1]]().dump(*sys.argv[2:])
